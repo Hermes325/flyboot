8521e82b0656b0dd61a8c34cf445196056d27bb9
unknown <maximguraevskiy@gmail.com>
2023-01-30 23:25:09 +0300

    bucket button

--- app/layouts/header.tsx
+++ app/layouts/header.tsx
@@ -6,6 +6,7 @@ import { Inter } from "@next/font/google";
 import NavLink from "./link";
 import classNames from "classnames";
 import logo_mini_path from "@/public/header-images/logo_mini.png";
+import bucket_logo from "@/public/header-images/bucket_logo.svg";
 // import menu_path from "@/public/header-images/menu.svg";
 // import menu_close_path from "@/public/header-images/close.svg";
 
@@ -20,33 +21,43 @@ const links = [
   { href: "#Как_это_работает?", label: "Как это работает?" },
   { href: "#A_what_with_delivery_?", label: "А что с доставкой?" },
   { href: "#About_us", label: "О нас" },
-  { href: "#Связаться", label: "Связаться" }
-]
+  { href: "#Связаться", label: "Связаться" },
+];
 
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <header className="fixed w-screen z-[1000] shadow">
+      {/* Logo and burger menu */}
       <nav className="flex flex-row items-center justify-between px-16 w-full h-[108px] bg-[#19191c] text-[#f9f9f9] transition z-[100] top-0 left-0">
         <NavLink href="/">
           <Image
             src={logo_mini_path}
             alt="Fly Boots Logo"
-            className="h-[55px] w-[55px]"
+            className="h-[55px] w-[70px]"
           />
         </NavLink>
 
-        <div
-          className={classNames(
-            "z-[3] grid place-items-center w-[20px] h-[20px] bg-[url('../public/header-images/menu.svg')] bg-no-repeat bg-center transition delay-150 duration-300 ease-in-out hover:animate-pulse",
-            {
-              "bg-[url('../public/header-images/close.svg')] transition":
-                isNavOpen,
-            }
-          )}
-          onClick={() => setIsNavOpen((prev) => !prev)}
-        />
+        <div className="flex flex-row justify-center items-center space-x-10">
+          <NavLink href="/Bucket">
+            <Image
+              src={bucket_logo}
+              alt="bucket page logo"
+              className="w-[50px] h-[50px]"
+            />
+          </NavLink>
+          <div
+            className={classNames(
+              "z-[3] grid place-items-center w-[20px] h-[20px] bg-[url('../public/header-images/menu.svg')] bg-no-repeat bg-center transition delay-150 duration-300 ease-in-out hover:animate-pulse",
+              {
+                "bg-[url('../public/header-images/close.svg')] transition":
+                  isNavOpen,
+              }
+            )}
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          />
+        </div>
       </nav>
 
       {/* Модальное окно навигации */}
@@ -57,7 +68,7 @@ function Header() {
         )}
         onClick={() => setIsNavOpen((prev) => !prev)}
       >
-        {links.map(({ href, label }, i) =>
+        {links.map(({ href, label }, i) => (
           <a
             className="text-[#f9f9f9] hover:text-[#00b5b5] text-5xl"
             style={{ animationDelay: `0.${i + 1}s` }}
@@ -65,7 +76,8 @@ function Header() {
             key={href}
           >
             {label}
-          </a>)}
+          </a>
+        ))}
       </nav>
     </header>
   );
