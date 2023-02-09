b38be0f2b6b343d14e5039291c4d1b8a68c06ed2
Vitaly <msk.vitaly@gmail.com>
2023-02-09 22:06:06 +0300

    fixes

--- lib/components/item_card.tsx
+++ lib/components/item_card.tsx
@@ -8,13 +8,13 @@ import "../../app/globals.css"
 
 type Props = {
   item: Item
-  className: string
+  className?: string
 }
 
 const ItemCard = ({ item, className }: Props) => {
   return (
     <Link href={`/item/${item.slug}`}>
-      <div className={`${styles.item_card} ${className} bg-[#003E3E] rounded-2xl flex flex-col p-[0.75vw] justify-between
+      <div className={`${styles.item_card} ${className ?? ""} bg-[#003E3E] rounded-2xl flex flex-col p-[0.75vw] justify-between
                       hover:bg-[#07b8b88e] hover:transition-[0.15s]`}>
         <Image
           lazyLoad={true}
