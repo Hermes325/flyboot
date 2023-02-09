710f16279aa159fbf400ee8fc3d4ff9ece0d8e3c
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-02-09 22:03:31 +0300

    mobile main

--- app/Landing/slide1.tsx
+++ app/Landing/slide1.tsx
@@ -10,15 +10,15 @@ function Slide1() {
     <section className="w-full px-[16.7vw]">
 
       <div className="mb-[10vw]">
-        <h1 className={styles.h1 + " font-inter"}>
+        <h1 className={styles.h1 + " font-inter max-[1860px]:text-[4.5rem] max-[1700px]:text-[4rem] max-[1500px]:text-[3.7rem]"}>
           Любимые бренды <br /> <span className={styles.span}>возвращаются</span>
         </h1>
-        <p className={styles.jost_offer + " font-jost"}>
+        <p className={styles.jost_offer + " font-jost max-[1860px]:text-[22px] max-[1700px]:text-[20px] max-[1500px]:text-[18px]"}>
           Если ты искал, где обновить гардероб, то ты в нужном месте. <br />
           Одевайся так, будто завтра выступаешь в Карнеги-Холл.
         </p>
         <form action="#catalog" className="mt-[1vw]">
-          <button className={styles.button_main + " font-montserrat p-[18px_30px_20px_30px]"}>
+          <button className={styles.button_main + " font-montserrat p-[18px_30px_20px_30px] max-[1860px]:text-[35px] max-[1700px]:text-[30px] max-[1500px]:text-[27px]"}>
             Смотреть каталог
           </button>
         </form>
@@ -28,20 +28,21 @@ function Slide1() {
         <Image
           src={waves}
           alt="Волна"
-          className={styles.waves} />
+          className={styles.waves + " max-[1750px]:top-[5%] max-[1700px]:top-[4%] max-[1550px]:top-[5%]" }
+        />
 
-        <div className={styles.krugmain}>
+        <div className={styles.krugmain + " max-[1860px]:w-[22vw]"}>
           <Image
             src={kross}
             alt="Логотип"
-            className={styles.Logotip} />
+            className={styles.Logotip + "  max-w-[unset] max-[1860px]:w-[24vw]"} />
         </div>
 
         <div>
-          <div className={styles.krug1} />
-          <div className={styles.krug2} />
-          <div className={styles.krug3} />
-          <div className={styles.krug4} />
+          <div className={styles.krug1 + " max-[1860px]:w-[7vw] max-[1550px]:w-[9vw] max-[1750px]:mt-[5%] max-[1700px]:mt-[0%] max-[1550px]:mt-[3%]"} />
+          <div className={styles.krug2 + " max-[1860px]:w-[9vw] max-[1550px]:w-[7vw] max-[1750px]:mt-[3%] max-[1700px]:mt-[1%] max-[1550px]:mt-[4%]"} />
+          <div className={styles.krug3 + " max-[1750px]:mt-[3%] max-[1700px]:mt-[0%] max-[1550px]:mt-[5%]"} />
+          <div className={styles.krug4 + " max-[1750px]:w-[6vw] max-[1750px]:mt-[5%] max-[1700px]:w-[7vw] max-[1550px]:w-[9vw] max-[1700px]:mt-[-1%] max-[1550px]:mt-[0%]"} />
         </div>
       </div>
 
