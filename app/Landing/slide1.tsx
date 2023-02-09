f0160e7de4ef12e8f55fc4a204603993b22db751
unknown <maximguraevskiy@gmail.com>
2023-02-09 23:39:47 +0300

    important fix main slide1 header

--- app/Landing/slide1.tsx
+++ app/Landing/slide1.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import waves from "@/public/main-images/waves.png";
 import kross from "@/public/main-images/14.webp";
 import styles from "./styles/slide1.module.css";
 
-
 function Slide1() {
   return (
     <section className="w-full px-[16.7vw]">
-
       <div className="mb-[10vw]">
-        <h1 className={styles.h1 + " font-inter max-[1860px]:text-[4.5rem] max-[1700px]:text-[4rem] max-[1500px]:text-[3.7rem] max-[1330px]:text-[3.3rem] max-[1140px]:text-[3rem]"}>
-          Любимые бренды <br /> <span className={styles.span}>возвращаются</span>
+        <h1
+          className={
+            styles.h1 +
+            " font-inter !max-[1860px]:text-[4.5rem] !max-[1700px]:text-[4rem] !max-[1500px]:text-[3.7rem] !max-[1330px]:text-[3.3rem] !max-[1140px]:text-[3rem]"
+          }
+        >
+          Любимые бренды <br />{" "}
+          <span className={styles.span}>возвращаются</span>
         </h1>
-        <p className={styles.jost_offer + " font-jost max-[1860px]:text-[22px] max-[1700px]:text-[20px] max-[1500px]:text-[18px] max-[1330px]:text-[16.5px] max-[1140px]:text-[15px]"}>
+        <p
+          className={
+            styles.jost_offer +
+            " font-jost max-[1860px]:text-[22px] max-[1700px]:text-[20px] max-[1500px]:text-[18px] max-[1330px]:text-[16.5px] max-[1140px]:text-[15px]"
+          }
+        >
           Если ты искал, где обновить гардероб, то ты в нужном месте. <br />
           Одевайся так, будто завтра выступаешь в Карнеги-Холл.
         </p>
         <form action="#catalog" className="mt-[1vw]">
-          <button className={styles.button_main + " font-montserrat p-[18px_30px_20px_30px] max-[1860px]:text-[35px] max-[1700px]:text-[30px] max-[1500px]:text-[27px] max-[1330px]:text-[25px] max-[1140px]:text-[22px]"}>
+          <button
+            className={
+              styles.button_main +
+              " font-montserrat p-[18px_30px_20px_30px] max-[1860px]:text-[35px] max-[1700px]:text-[30px] max-[1500px]:text-[27px] max-[1330px]:text-[25px] max-[1140px]:text-[22px]"
+            }
+          >
             Смотреть каталог
           </button>
         </form>
@@ -28,24 +42,52 @@ function Slide1() {
         <Image
           src={waves}
           alt="Волна"
-          className={styles.waves + " max-[1750px]:top-[5%] max-[1700px]:top-[4%] max-[1550px]:top-[5%]" }
+          className={
+            styles.waves +
+            " max-[1750px]:top-[5%] max-[1700px]:top-[4%] max-[1550px]:top-[5%]"
+          }
         />
 
-        <div className={styles.krugmain + " max-[1860px]:w-[22vw] max-[1400px]:left-[65%] max-[1025px]:top-[4%]" }>
+        <div
+          className={
+            styles.krugmain +
+            " max-[1860px]:w-[22vw] max-[1400px]:left-[65%] max-[1025px]:top-[4%]"
+          }
+        >
           <Image
             src={kross}
             alt="Логотип"
-            className={styles.Logotip + "  max-w-[unset] max-[1860px]:w-[24vw]"} />
+            className={styles.Logotip + "  max-w-[unset] max-[1860px]:w-[24vw]"}
+          />
         </div>
 
         <div>
-          <div className={styles.krug1 + " max-[1860px]:w-[7vw] max-[1550px]:w-[9vw] max-[1750px]:mt-[5%] max-[1700px]:mt-[0%] max-[1550px]:mt-[3%] max-[1200px]:mt-[5%]"} />
-          <div className={styles.krug2 + " max-[1860px]:w-[9vw] max-[1550px]:w-[7vw] max-[1750px]:mt-[3%] max-[1700px]:mt-[1%] max-[1550px]:mt-[4%] max-[1200px]:mt-[9%] max-[1025px]:mt-[13%]"} />
-          <div className={styles.krug3 + " max-[1750px]:mt-[3%] max-[1700px]:mt-[0%] max-[1550px]:mt-[5%] max-[1200px]:mt-[9%]"} />
-          <div className={styles.krug4 + " max-[1750px]:w-[6vw] max-[1750px]:mt-[5%] max-[1700px]:w-[7vw] max-[1550px]:w-[9vw] max-[1700px]:mt-[-1%] max-[1550px]:mt-[0%] max-[1450px]:w-[7vw] max-[1300px]:mt-[13%]"} />
+          <div
+            className={
+              styles.krug1 +
+              " max-[1860px]:w-[7vw] max-[1550px]:w-[9vw] max-[1750px]:mt-[5%] max-[1700px]:mt-[0%] max-[1550px]:mt-[3%] max-[1200px]:mt-[5%]"
+            }
+          />
+          <div
+            className={
+              styles.krug2 +
+              " max-[1860px]:w-[9vw] max-[1550px]:w-[7vw] max-[1750px]:mt-[3%] max-[1700px]:mt-[1%] max-[1550px]:mt-[4%] max-[1200px]:mt-[9%] max-[1025px]:mt-[13%]"
+            }
+          />
+          <div
+            className={
+              styles.krug3 +
+              " max-[1750px]:mt-[3%] max-[1700px]:mt-[0%] max-[1550px]:mt-[5%] max-[1200px]:mt-[9%]"
+            }
+          />
+          <div
+            className={
+              styles.krug4 +
+              " max-[1750px]:w-[6vw] max-[1750px]:mt-[5%] max-[1700px]:w-[7vw] max-[1550px]:w-[9vw] max-[1700px]:mt-[-1%] max-[1550px]:mt-[0%] max-[1450px]:w-[7vw] max-[1300px]:mt-[13%]"
+            }
+          />
         </div>
       </div>
-
     </section>
   );
 }
