928cbefd4ad26c650f76de15ad19ef6838412f08
unknown <maximguraevskiy@gmail.com>
2023-02-10 02:06:25 +0300

    header adaptive

--- lib/components/item_card.tsx
+++ lib/components/item_card.tsx
@@ -1,37 +1,43 @@
-"use client"
-import React from 'react'
-import { Item } from '@/lib/datocms'
+"use client";
+import React from "react";
+import { Item } from "@/lib/datocms";
 import { Image } from "react-datocms";
-import Link from 'next/link';
-import styles from "./item_card.module.css"
-import "../../app/globals.css"
+import Link from "next/link";
+import styles from "./item_card.module.css";
+import "../../app/globals.css";
 
 type Props = {
-  item: Item
-  className?: string
-}
+  item: Item;
+  className?: string;
+};
 
 const ItemCard = ({ item, className }: Props) => {
   return (
     <Link href={`/item/${item.slug}`}>
-      <div className={`${styles.item_card} ${className ?? ""} bg-[#003E3E] rounded-2xl flex flex-col p-[0.75vw] justify-between
-                      hover:bg-[#07b8b88e] hover:transition-[0.15s]`}>
+      <div
+        className={`${styles.item_card} ${
+          className ?? ""
+        } bg-[#003E3E] rounded-2xl flex flex-col p-[0.75vw] justify-between
+                      hover:bg-[#07b8b88e] hover:transition-[0.15s]`}
+      >
         <Image
           lazyLoad={true}
-          objectFit={'cover'}
+          // objectFit={"cover"}
           data={item.images[0].responsiveImage}
-          style={{ maxWidth: "none" }}
-          className='rounded-xl h-[75%] w-full' />
+          // style={{ maxWidth: "none" }}
+          className="rounded-xl h-[75%] w-full"
+        />
 
         <div>
-          <h3 className={styles['name']}>{item.title}</h3>
-          <p className={styles['price']}>{item.price}&nbsp;руб</p>
+          <h3 className={styles["name"]}>{item.title}</h3>
+          <p className={styles["price"]}>{item.price}&nbsp;руб</p>
         </div>
         {/* <button className={styles['buy-btn']}>
           Купить
         </button> */}
       </div>
-    </Link>)
-}
+    </Link>
+  );
+};
 
-export default ItemCard
+export default ItemCard;
