a2b7dbc1069e485536cc10b0c39bf1a799e07df9
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-02-09 23:47:12 +0300

    mobile slide fix again

--- app/Landing/slide1.tsx
+++ app/Landing/slide1.tsx
@@ -20,7 +20,7 @@ function Slide1() {
         <p
           className={
             styles.jost_offer +
-            " font-jost max-[1860px]:text-[22px] max-[1700px]:text-[20px] max-[1500px]:text-[18px] max-[1330px]:text-[16.5px] max-[1140px]:text-[15px]"
+            " font-jost max-[1860px]:!text-[22px] max-[1700px]:!text-[20px] max-[1500px]:!text-[18px] max-[1330px]:!text-[16.5px] max-[1140px]:!text-[15px]"
           }
         >
           Если ты искал, где обновить гардероб, то ты в нужном месте. <br />
@@ -30,7 +30,7 @@ function Slide1() {
           <button
             className={
               styles.button_main +
-              " font-montserrat p-[18px_30px_20px_30px] max-[1860px]:text-[35px] max-[1700px]:text-[30px] max-[1500px]:text-[27px] max-[1330px]:text-[25px] max-[1140px]:text-[22px]"
+              " font-montserrat p-[18px_30px_20px_30px] max-[1860px]:!text-[35px] max-[1700px]:!text-[30px] max-[1500px]:!text-[27px] max-[1330px]:!text-[25px] max-[1140px]:!text-[22px]"
             }
           >
             Смотреть каталог
@@ -44,20 +44,20 @@ function Slide1() {
           alt="Волна"
           className={
             styles.waves +
-            " max-[1750px]:top-[5%] max-[1700px]:top-[4%] max-[1550px]:top-[5%]"
+            " max-[1750px]:!top-[5%] max-[1700px]:!top-[4%] max-[1550px]:!top-[5%]"
           }
         />
 
         <div
           className={
             styles.krugmain +
-            " max-[1860px]:w-[22vw] max-[1400px]:left-[65%] max-[1025px]:top-[4%]"
+            " max-[1860px]:!w-[22vw] max-[1400px]:!left-[65%] max-[1025px]:!top-[4%]"
           }
         >
           <Image
             src={kross}
             alt="Логотип"
-            className={styles.Logotip + "  max-w-[unset] max-[1860px]:w-[24vw]"}
+            className={styles.Logotip + "  max-w-[unset] max-[1860px]:!w-[24vw]"}
           />
         </div>
 
@@ -65,25 +65,25 @@ function Slide1() {
           <div
             className={
               styles.krug1 +
-              " max-[1860px]:w-[7vw] max-[1550px]:w-[9vw] max-[1750px]:mt-[5%] max-[1700px]:mt-[0%] max-[1550px]:mt-[3%] max-[1200px]:mt-[5%]"
+              " max-[1860px]:!w-[7vw] max-[1550px]:!w-[9vw] max-[1750px]:!mt-[5%] max-[1700px]:!mt-[0%] max-[1550px]:!mt-[3%] max-[1200px]:!mt-[5%]"
             }
           />
           <div
             className={
               styles.krug2 +
-              " max-[1860px]:w-[9vw] max-[1550px]:w-[7vw] max-[1750px]:mt-[3%] max-[1700px]:mt-[1%] max-[1550px]:mt-[4%] max-[1200px]:mt-[9%] max-[1025px]:mt-[13%]"
+              " max-[1860px]:!w-[9vw] max-[1550px]:!w-[7vw] max-[1750px]:!mt-[3%] max-[1700px]:!mt-[1%] max-[1550px]:!mt-[4%] max-[1200px]:!mt-[9%] max-[1025px]:!mt-[13%]"
             }
           />
           <div
             className={
               styles.krug3 +
-              " max-[1750px]:mt-[3%] max-[1700px]:mt-[0%] max-[1550px]:mt-[5%] max-[1200px]:mt-[9%]"
+              " max-[1750px]:!mt-[3%] max-[1700px]:!mt-[0%] max-[1550px]:!mt-[5%] max-[1200px]:!mt-[9%]"
             }
           />
           <div
             className={
               styles.krug4 +
-              " max-[1750px]:w-[6vw] max-[1750px]:mt-[5%] max-[1700px]:w-[7vw] max-[1550px]:w-[9vw] max-[1700px]:mt-[-1%] max-[1550px]:mt-[0%] max-[1450px]:w-[7vw] max-[1300px]:mt-[13%]"
+              " max-[1750px]:!w-[6vw] max-[1750px]:!mt-[5%] max-[1700px]:!w-[7vw] max-[1550px]:!w-[9vw] max-[1700px]:!mt-[-1%] max-[1550px]:!mt-[0%] max-[1450px]:!w-[7vw] max-[1300px]:!mt-[13%]"
             }
           />
         </div>
