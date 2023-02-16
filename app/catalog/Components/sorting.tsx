360f2b259fe4699bc1c2d57f20d6d113d4460e95
Kolbak <kushaevmarat@mail.ru>
2023-02-16 22:37:42 +0300

    GENIUS PROBLEM SOLUTION !!!

--- app/catalog/Components/sorting.tsx
+++ app/catalog/Components/sorting.tsx
@@ -26,9 +26,9 @@ const Sorting = ({ filters, setFiltersWrapper }: Props) => {
       onChange={(x) => setFiltersWrapper(changeSort(Number(x.target.value)))}
       className="bg-[#0E0E0E] font-inter text-white text-[16px] border border-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
     >
-      <option className='text-[#9A9A9A] text-[16px]' value={SortType.default}>Сортировка: по умолчанию</option>
-      <option className='text-[#9A9A9A] text-[16px]' value={SortType.price_ASC}>Сортировка: по возрастанию цены</option>
-      <option className='text-[#9A9A9A] text-[16px]' value={SortType.price_DESC}>Сортировка: по убыванию цены</option>
+      <option className='text-[#9A9A9A] text-[16px]' value={SortType.default}>Без сортировки</option>
+      <option className='text-[#9A9A9A] text-[16px]' value={SortType.price_ASC}>По возрастанию цены</option>
+      <option className='text-[#9A9A9A] text-[16px]' value={SortType.price_DESC}>По убыванию цены</option>
     </select>
   )
 }
