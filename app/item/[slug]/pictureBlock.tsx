5aabd1e88132d90483d59d626d107aa98d647fec
Vitaly <msk.vitaly@gmail.com>
2023-02-16 21:36:38 +0300

    pictures

--- app/item/[slug]/pictureBlock.tsx
+++ app/item/[slug]/pictureBlock.tsx
@@ -15,7 +15,7 @@ const PictureBlock = ({ item }: Props) => {
 
   return (<>
     <Image
-      data={images[0].image.responsiveImage}
+      data={images.find(x => x.index === 0)?.image.responsiveImage}
       className="rounded-[15px]"
       style={{ maxWidth: "none" }}
       pictureClassName="object-cover aspect-[646/532] h-[532px] rounded-[15px]" />
@@ -30,16 +30,14 @@ const PictureBlock = ({ item }: Props) => {
             key={`small-${index}`}
             className="rounded-[15px] cursor-pointer"
             onClick={_ => {
-              // Меняем номер картинки на current
-              setImages(x => x.map(_x => {
-                switch (_x.index) {
-                  case 0: return { ..._x, index }
-                  case index: return { ..._x, index: 0 }
-                  default: return _x
-                }
-              }))
-              // Устанавливаем новый current
-              // setCurrent(index)
+              setImages(oldImgs => {
+                let imgs = [...oldImgs];
+                // Меняем элементы и устанавливаем индексы
+                [imgs[0], imgs[index]] = [imgs[index], imgs[0]];
+                imgs[0].index = 0
+                imgs[index].index = index
+                return imgs
+              })
             }}>
             <Image
               lazyLoad={true}
