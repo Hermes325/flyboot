34fd1e1df34ad0849a7780b79add99beb671a3f5
unknown <maximguraevskiy@gmail.com>
2023-02-18 01:03:53 +0300

    modal search

--- app/layouts/headerSearchList.tsx
+++ app/layouts/headerSearchList.tsx
@@ -1,5 +1,6 @@
 import { Item } from "@/lib/datocms";
 import React from "react";
+import { Image } from "react-datocms/image";
 import NavLink from "./link";
 
 type Props = {
@@ -12,7 +13,19 @@ const HeaderSearchList = ({ items }: Props) => {
       {items.map((item) => (
         <li key={item.slug}>
           <NavLink href={`/catalog/${item.slug}`}>
-            <p className="">{item.title}</p>
+            <div className="flex flex-row w-screen px-10 justify-between ">
+              <div className="space-x-3">
+                {/* <Image
+                  lazyLoad={true}
+                  data={item.images[0].responsiveImage}
+                  objectFit={"cover"}
+                  className="w-[30px] h-[30px]"
+                /> */}
+                <p className="">{item.title}</p>
+              </div>
+
+              <p className="">{item.price}руб</p>
+            </div>
           </NavLink>
         </li>
       ))}
