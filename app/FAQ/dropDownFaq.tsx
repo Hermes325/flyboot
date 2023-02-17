cc844d18c53f4800966a06a3c005db802698f5da
Vitaly <msk.vitaly@gmail.com>
2023-02-17 22:43:24 +0300

    dropDownFaq

--- app/FAQ/dropDownFaq.tsx
+++ app/FAQ/dropDownFaq.tsx
@@ -5,38 +5,39 @@ import classNames from "classnames";
 import expand from "@/public/arrow/ArrowDropdown.svg"
 
 type Props = {
-  title: string;
-  description: string;
-  open?: boolean;
+  title: string
+  open?: boolean
+  children: string | JSX.Element | JSX.Element[]
 };
 
-function DropDown({ title, description, open = false }: Props) {
+function DropDown({ title, children, open = false }: Props) {
   const [isOpen, setIsOpen] = useState(open);
-  return (
-    <div className={classNames(`flex flex-col w-[80%] border-b-2 border-white
+
+  return (<section className={classNames(`flex flex-col w-[80%] border-b-2 border-white
        max-[600px]:w-[100%]`, {
-      "border-white pb-[20px]": isOpen,
-    })}>
-      <div
-        className="flex flex-row justify-between items-center cursor-pointer"
-        onClick={() => setIsOpen(prev => !prev)}
-      >
-        <h2 className="font-lato font-[400] text-white text-[20px] leading-[27px] tracking-[0.01em]">{title}</h2>
-        <Image
-          src={expand}
-          alt="expand less image"
-          className={classNames("select-none transition-transform h-[50px]", {
-            "rotate-180": isOpen
-          })} />
-      </div>
-      <div className={classNames({ "opacity-0 invisible": !isOpen })}>
-        <p className={classNames("font-lato font-[400] text-white text-[15px] leading-[25px] tracking-[0.01em]", {
-          "hidden": !isOpen
-        })}>
-          {description}
-        </p>
-      </div>
-    </div>)
+    "border-white pb-[20px]": isOpen,
+  })}>
+
+    {/* Заголовок */}
+    <div
+      className="flex flex-row justify-between items-center cursor-pointer"
+      onClick={() => setIsOpen(prev => !prev)}
+    >
+      <h2 className="font-lato font-[400] text-white text-[20px] leading-[27px] tracking-[0.01em]">{title}</h2>
+      <Image
+        src={expand}
+        alt="expand less image"
+        className={classNames("select-none transition-transform h-[50px]", {
+          "rotate-180": isOpen
+        })} />
+    </div>
+
+    {/* Внутренний контент */}
+    <div className={classNames({ "hidden": !isOpen })}>
+      {children}
+    </div>
+
+  </section>)
 }
 
 export default DropDown;
