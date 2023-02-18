6348accfc3d2f404703d0f50ca36830bc4ad50fe
Vitaly <msk.vitaly@gmail.com>
2023-02-19 00:22:27 +0300

    Страница после оплаты

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+
+const ThankPage = () => {
+  return (<main className='min-h-screen flex flex-col items-center justify-start pt-[158px]'>
+    <h1 className='font-montserrat'>Благодарим за покупку ♥</h1>
+    <p className='font-roboto'>Мы отправили чек на вашу почту</p>
+    <address>
+      <p>За помощью с заказом обращайтесь {"<... Куда? ...>"}</p>
+    </address>
+
+    <Link className='text-white underline' href='/catalog'>
+      Вернуться на каталог
+    </Link>
+  </main>)
+}
+
+export default ThankPage
