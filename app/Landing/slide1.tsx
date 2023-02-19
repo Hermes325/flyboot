6a83dc2313b353ab07b15395724556c886a2f477
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-02-19 17:57:04 +0300

    AU mob

--- app/Landing/slide1.tsx
+++ app/Landing/slide1.tsx
@@ -11,7 +11,7 @@ function Slide1() {
         <h1
           className={
             styles.h1 +
-            " font-inter min-[2560px]:!text-[6.5rem] max-[1860px]:!text-[4.5rem] max-[1700px]:!text-[4rem] max-[1500px]:!text-[3.7rem] max-[1330px]:!text-[3.3rem] max-[1140px]:!text-[3rem]"
+            " font-inter min-[2560px]:!text-[6.5rem] max-[1860px]:!text-[4.5rem] max-[1700px]:!text-[4rem] max-[1500px]:!text-[3.7rem] max-[1330px]:!text-[3.3rem] max-[1140px]:!text-[3rem] max-[600px]:!text-[2.5rem]"
           }
         >
           Любимые бренды <br />{" "}
@@ -20,17 +20,18 @@ function Slide1() {
         <p
           className={
             styles.jost_offer +
-            " font-jost min-[2560px]:!text-[32px] min-[2560px]:!leading-[42px] max-[1860px]:!text-[22px] max-[1700px]:!text-[20px] max-[1500px]:!text-[18px] max-[1330px]:!text-[16.5px] max-[1140px]:!text-[15px] "
+            " font-jost min-[2560px]:!text-[32px] min-[2560px]:!leading-[42px] max-[1860px]:!text-[22px] max-[1700px]:!text-[20px] max-[1500px]:!text-[18px] max-[1330px]:!text-[16.5px] max-[1140px]:!text-[15px] max-[600px]:!text-[1.2rem] max-[600px]:!leading-[17px] max-[600px]:!w-[90%] min-[601px]:!w-[25%]"
           }
         >
-          Если ты искал, где обновить гардероб, то ты в нужном месте. <br />
           Одевайся так, будто завтра выступаешь в Карнеги-Холл.
+          Если ты искал, где обновить гардероб, то ты в нужном месте.
+          
         </p>
         <a href="/catalog" className="mt-[1vw]">
           <button
             className={
               styles.button_main +
-              " font-montserrat p-[18px_30px_20px_30px] min-[2560px]:!text-[55px] min-[2560px]:!p-[30px_30px_30px_30px] max-[1860px]:!text-[35px] max-[1700px]:!text-[30px] max-[1500px]:!text-[27px] max-[1330px]:!text-[25px] max-[1140px]:!text-[22px]"
+              " font-montserrat p-[18px_30px_20px_30px] min-[2560px]:!text-[55px] min-[2560px]:!p-[30px_30px_30px_30px] max-[1860px]:!text-[35px] max-[1700px]:!text-[30px] max-[1500px]:!text-[27px] max-[1330px]:!text-[25px] max-[1140px]:!text-[22px] max-[600px]:!text-[15px] max-[600px]:!p-[3px_25px_3px_25px]"
             }
           >
             Смотреть каталог
@@ -44,26 +45,26 @@ function Slide1() {
           alt="Волна"
           className={
             styles.waves +
-            " max-[1750px]:!top-[5%] max-[1700px]:!top-[5%] max-[1550px]:!top-[6%]"
+            " max-[1750px]:!top-[5%] max-[1700px]:!top-[5%] max-[1550px]:!top-[6%] min-[601px]:!top-[10%] max-[600px]:!top-[25%] max-[1860px]:!w-[100%]"
           }
         />
 
         <div
           className={
             styles.krugmain +
-            " max-[1860px]:!w-[22vw] max-[1400px]:!left-[65%] max-[1025px]:!top-[4%]"
+            " max-[1860px]:!w-[22vw] max-[1400px]:!left-[65%] max-[1025px]:!top-[4%] max-[600px]:!top-[35%] max-[600px]:!left-[30%] max-[600px]:!w-[40%] max-[600px]:!w-[50vw]"
           }
         >
           <Image
             src={kross}
             alt="Логотип"
             className={
-              styles.Logotip + "  max-w-[unset] max-[1860px]:!w-[24vw]"
+              styles.Logotip + "  max-w-[unset] max-[1860px]:!w-[24vw] max-[600px]:!w-[60vw] max-[600px]:!bottom-[15vw] "
             }
           />
         </div>
 
-        <div>
+        <div className="max-[600px]:!hidden">
           <div
             className={
               styles.krug1 +
--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import Slide3 from "./Landing/slide3";
 import Slide4 from "./Landing/slide4";
 
 const Landing = () => (
-  <main className="min-h-screen flex flex-col items-center justify-start pt-[158px]">
+  <main className="min-h-screen flex flex-col items-center justify-start pt-[158px] max-[600px]:!pt-[110px]">
     {/* Entry page slide */}
     <Slide1 />
     {/* Demo Catalog Boots */}
-    <Slide2 />
+    {/* <Slide2 /> */}
     {/* Catalogs with category filters  */}
-    <Slide3 />
+    {/* <Slide3 /> */}
     {/* How it works */}
-    <Slide4 />
+    {/* <Slide4 /> */}
   </main>
 );
 
