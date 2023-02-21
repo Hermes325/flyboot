3a96b1edacd084f9efc780093dda78dc82ce1422
Kolbak <kushaevmarat@mail.ru>
2023-02-21 20:17:32 +0300

    set only one active tab in FAQ

--- app/FAQ/dropDownFaq.tsx
+++ app/FAQ/dropDownFaq.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactElement, useState } from "react";
+import React, {ReactElement, useEffect, useState} from "react";
 import Image from "next/image";
 import classNames from "classnames";
 import expand from "@/public/arrow/ArrowDropdown.svg"
@@ -8,10 +8,22 @@ type Props = {
   title: string
   open?: boolean
   children: string | JSX.Element | JSX.Element[]
+  activeTab: string
+  setActiveTab: Function
 };
 
-function DropDown({ title, children, open = false }: Props) {
+function DropDown({ title, children, open = false, activeTab, setActiveTab }: Props) {
   const [isOpen, setIsOpen] = useState(open);
+  function changeTab() {
+    setIsOpen(prev => !prev)
+    setActiveTab(title)
+  }
+
+  useEffect(() => {
+    if (activeTab !== title) {
+      setIsOpen(false)
+    }
+  }, [activeTab])
 
   return (<section className={classNames(`flex flex-col w-[100%] border-b-2 border-white
        max-[600px]:w-[100%] mt-2 `, {
@@ -21,7 +33,7 @@ function DropDown({ title, children, open = false }: Props) {
     {/* Заголовок */}
     <div
       className="flex flex-row justify-between items-center cursor-pointer mb-2"
-      onClick={() => setIsOpen(prev => !prev)}
+      onClick={() => changeTab()}
     >
       <h2 className="font-lato font-[400] text-[#29D9CE] text-[1.8rem] leading-[45px] tracking-[0.01em] max-[1000px]:text-[1.8rem] max-[800px]:text-[1.5rem] max-[600px]:text-[1.2rem] max-[600px]:leading-[25px]">{title}</h2>
       <Image
