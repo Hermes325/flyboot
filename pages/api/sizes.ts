d734dbfdfdcc0e6ca5b8aa025c36938f9c1bbfc3
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-03-04 14:35:09 +0300

    size out

--- pages/api/sizes.ts
+++ pages/api/sizes.ts
@@ -8,28 +8,28 @@ export type Sizes = {
   available: boolean[]
 }[]
 
-// const mock: Sizes = [
-//   {
-//     sizeKey: "EU",
-//     sizeValue: ["35.5", "36", "36.5", "37.5", "38", "38.5", "39", "40", "40.5", "41", "42", "42.5", "43", "44", "44.5", "45", "45.5", "46", "47", "47.5", "48", "48.5", "49.5", "50.5", "51.5"],
-//     available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
-//   },
-//   {
-//     sizeKey: "US",
-//     sizeValue: ["3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14", "15", "16", "17"],
-//     available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
-//   },
-//   {
-//     sizeKey: "UK",
-//     sizeValue: ["3", "3.5", "4", "4.5", "5", "5.5", "6", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "14", "15", "16"],
-//     available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
-//   },
-//   {
-//     sizeKey: "CM",
-//     sizeValue: ["22.5", "23", "23.5", "23.5", "24", "24", "24.5", "25", "25.5", "26", "26.5", "27", "27.5", "28", "28.5", "29", "29.5", "30", "30.5", "31", "31.5", "32", "33", "34", "35"],
-//     available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
-//   }
-// ]
+const mock: Sizes = [
+  // {
+  //   sizeKey: "EU",
+  //   sizeValue: ["35.5", "36", "36.5", "37.5", "38", "38.5", "39", "40", "40.5", "41", "42", "42.5", "43", "44", "44.5", "45", "45.5", "46", "47", "47.5", "48", "48.5", "49.5", "50.5", "51.5"],
+  //   available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
+  // },
+  // {
+  //   sizeKey: "US",
+  //   sizeValue: ["3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14", "15", "16", "17"],
+  //   available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
+  // },
+  // {
+  //   sizeKey: "UK",
+  //   sizeValue: ["3", "3.5", "4", "4.5", "5", "5.5", "6", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "14", "15", "16"],
+  //   available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
+  // },
+  // {
+  //   sizeKey: "CM",
+  //   sizeValue: ["22.5", "23", "23.5", "23.5", "24", "24", "24.5", "25", "25.5", "26", "26.5", "27", "27.5", "28", "28.5", "29", "29.5", "30", "30.5", "31", "31.5", "32", "33", "34", "35"],
+  //   available: [false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false]
+  // }
+]
 
 //* Функция Егора
 export default async function sizes(
