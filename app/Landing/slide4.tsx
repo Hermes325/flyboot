8d22deeb9a267f7f0082f861ac7bb23651b4c1c0
Hermes325 <112727212+Hermes325@users.noreply.github.com>
2023-05-01 15:25:26 +0300

    HIW

--- app/Landing/slide4.tsx
+++ app/Landing/slide4.tsx
@@ -46,26 +46,27 @@ function Slide4() {
 
 
 
-      <div className="grid grid-cols-3 max-[1300px]:!grid-cols-2 max-[600px]:!grid-cols-1 gap-5 text-center justify-items-center my-8 gap-x-4">
-        <div className='flex flex-col gap-y-8 border-solid border-2 rounded-xl border-[#08DAD4] bg-black max-[600px]:!w-[85%]'>
-          <div className={styles.flex_items_center + " justify-center mt-10"}>
+      <div className="grid grid-cols-4 max-[600px]:!grid-cols-1 gap-5 text-center justify-items-start my-8 gap-x-4">
+
+        <div className='flex flex-col gap-y-8 shadow_border rounded-[10px] bg-[#fff] max-[600px]:!w-[85%]'>
+          <div className={styles.flex_items_center + " justify-start mt-10"}>
             <Image
               src={recept}
               alt="recept"
               className="w-[3.6vw] max-[600px]:!w-[5vw] max-[500px]:!w-[6vw] max-[400px]:!w-[8vw]" />
             &ensp;&ensp;
-            <p className={styles.hiw_list + ` text-center noto_offer
+            <p className={styles.hiw_list + ` uppercase text-start noto_offer text-black
            max-[2000px]:!text-[30px] 
            max-[1700px]:!text-[28px]
            max-[1100px]:!text-[25px] 
            max-[950px]:!text-[21px]
-           max-[1100px]:!leading-[17px] font-bold`}>Ваш заказ</p>
+           max-[1100px]:!leading-[17px] font-bold`}>оплата</p>
           </div>
-
           <p className={styles.hiw_list + ` noto_offer
            mb-10
            px-5
-           text-center
+           text-black
+           text-start
            max-[2000px]:!text-[24px] 
            max-[2000px]:!leading-[25px]
            max-[1700px]:!text-[20px]
@@ -74,31 +75,29 @@ function Slide4() {
            max-[780px]:!text-[12px] 
            max-[600px]:!text-[16px] 
            max-[1100px]:!leading-[17px]`}>
-            План такой: в каталоге ищете то, что радует глаз,
-            оплачиваете и получаете чек вместе с карточкой-подтверждением о выкупе товара.
+            Оплата заказа и получение карточки-подтверждения о выкупе товара
 
           </p>
         </div>
 
-        <div className='flex flex-col gap-y-8 border-solid border-2 rounded-xl border-[#08DAD4] bg-black max-[600px]:!w-[85%]'>
-          <div className={styles.flex_items_center + " justify-center mt-10"}>
+        <div className='flex flex-col gap-y-8 shadow_border rounded-[10px] bg-[#fff] max-[600px]:!w-[85%]'>
+          <div className={styles.flex_items_center + " justify-start mt-10"}>
             <Image
               src={shopping_bag}
               alt="shopping_bag"
               className="w-[3.6vw] max-[600px]:!w-[5vw] max-[500px]:!w-[6vw] max-[400px]:!w-[8vw]" />
             &ensp;&ensp;
-            <p className={styles.hiw_list + ` text-center font-bold noto_offer
+            <p className={styles.hiw_list + ` uppercase text-center font-bold noto_offer
            max-[2000px]:!text-[30px] 
            max-[1700px]:!text-[28px]
            max-[1100px]:!text-[25px]
            max-[950px]:!text-[21px]        
-           max-[1100px]:!leading-[17px]`}>Сбор заказа</p>
+           max-[1100px]:!leading-[17px] text-black`}>Подготовка</p>
           </div>
-
-          <p className={styles.hiw_list + ` noto_offer
+          <p className={styles.hiw_list + `  text-black  noto_offer
            mb-10
            px-5
-           text-center
+           text-start
            max-[2000px]:!text-[24px] 
            max-[2000px]:!leading-[25px]
            max-[1700px]:!text-[20px]
@@ -107,36 +106,35 @@ function Slide4() {
            max-[780px]:!text-[12px] 
            max-[600px]:!text-[16px] 
            max-[1100px]:!leading-[17px]`}>
-            Наши коллеги в Китае ворожат над вашим заказом,
-            проверяют, чтобы все было по высшему разряду и упаковывают так,
-            чтобы не стыдно было потом в Инстаграмчик залить.
+            На данном этапе ваш заказ проходит подготовку
+            к отправке в стране отправления
           </p>
         </div>
 
-        <div className='flex flex-col gap-y-8 border-solid border-2 rounded-xl border-[#08DAD4] bg-black max-[600px]:!w-[85%] 
+        <div className='flex flex-col gap-y-8 shadow_border rounded-[10px] bg-[#fff] max-[600px]:!w-[85%] 
        min-[1300px]:!col-end-auto 
        min-[1300px]:!col-start-auto
        min-[600px]:!col-end-3 
         min-[600px]:!col-start-1
         '>
-          <div className={styles.flex_items_center + " justify-center mt-10"}>
+          <div className={styles.flex_items_center + " justify-start mt-10"}>
             <Image
               src={carbon_delivery_parcel}
               alt="carbon_delivery-parcel"
               className="w-[3.6vw] max-[600px]:!w-[5vw] max-[500px]:!w-[6vw] max-[400px]:!w-[8vw]" />
             &ensp;&ensp;
-            <p className={styles.hiw_list + ` font-bold text-center noto_offer
+            <p className={styles.hiw_list + ` text-black uppercase font-bold text-center noto_offer
            max-[2000px]:!text-[30px] 
            max-[1700px]:!text-[28px]
            max-[1100px]:!text-[25px] 
            max-[950px]:!text-[21px]           
-           max-[1100px]:!leading-[17px]`}>Доставка</p>
+           max-[1100px]:!leading-[17px]`}>отправление</p>
           </div>
 
-          <p className={styles.hiw_list + ` noto_offer
+          <p className={styles.hiw_list + ` text-black noto_offer
           px-5
            mb-10
-           text-center
+           text-start
            max-[2000px]:!text-[24px] 
            max-[2000px]:!leading-[25px]
            max-[1700px]:!text-[20px]
@@ -145,9 +143,44 @@ function Slide4() {
            max-[780px]:!text-[12px] 
            max-[600px]:!text-[16px] 
            max-[1100px]:!leading-[17px]`}>
-            Посылка со склада отправляется в Россию на ковре-самолёте,
-            вся красивая, и застрахованная. В течение 2-3 недель она оказывается
-            у своего счастливого обладателя!</p>
+            Посылка со склада отправляется в Россию,
+            ориентировочный срок доставки 2-3 недели</p>
+        </div>
+
+        <div className='flex flex-col gap-y-8 shadow_border rounded-[10px] bg-[#fff] max-[600px]:!w-[85%] 
+       min-[1300px]:!col-end-auto 
+       min-[1300px]:!col-start-auto
+       min-[600px]:!col-end-3 
+        min-[600px]:!col-start-1
+        '>
+          <div className={styles.flex_items_center + " justify-start mt-10"}>
+            <Image
+              src={carbon_delivery_parcel}
+              alt="carbon_delivery-parcel"
+              className="w-[3.6vw] max-[600px]:!w-[5vw] max-[500px]:!w-[6vw] max-[400px]:!w-[8vw]" />
+            &ensp;&ensp;
+            <p className={styles.hiw_list + ` text-black uppercase font-bold text-center noto_offer
+           max-[2000px]:!text-[30px] 
+           max-[1700px]:!text-[28px]
+           max-[1100px]:!text-[25px] 
+           max-[950px]:!text-[21px]           
+           max-[1100px]:!leading-[17px]`}>ПОЛУЧЕНИЕ</p>
+          </div>
+
+          <p className={styles.hiw_list + `  text-black noto_offer
+          px-5
+           mb-10
+           text-start
+           max-[2000px]:!text-[24px] 
+           max-[2000px]:!leading-[25px]
+           max-[1700px]:!text-[20px]
+           max-[1100px]:!text-[16px] 
+           max-[850px]:!text-[14px] 
+           max-[780px]:!text-[12px] 
+           max-[600px]:!text-[16px] 
+           max-[1100px]:!leading-[17px]`}>
+            Посылка прибыла в Россию
+            и ее можно забирать</p>
         </div>
 
       </div>
